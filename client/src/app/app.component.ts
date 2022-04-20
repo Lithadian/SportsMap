3d845140887ad23b5ff48327c8a6811c912535bd
Andrejs.Kuznecovs <andrejs.kuznecovs@youforce.net>
2022-04-20 09:32:54 +0300

    +front google auth

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,22 +1,50 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  SocialAuthService,
+  GoogleLoginProvider,
+  SocialUser,
+} from 'angularx-social-login';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
+  loginForm!: FormGroup;
+  socialUser!: SocialUser;
+  isLoggedin?: boolean;
   title = 'The Sports Map';
   users: any;
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, 
+    private formBuilder: FormBuilder,
+    private socialAuthService: SocialAuthService){}
+  
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+    this.socialAuthService.authState.subscribe((user) => {
+      this.socialUser = user;
+      this.isLoggedin = user != null;
+      console.log(this.socialUser);
+    });
     this.getUsers();
+    
   }
-
-
+  loginWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+  logOut(): void {
+    this.socialAuthService.signOut();
+  }
+  
   getUsers(){
     this.http.get('https://localhost:5001/api/User/GetUserlist').subscribe(response => {
       console.log(this.users = response);
